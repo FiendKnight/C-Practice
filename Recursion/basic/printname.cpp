b666196909d8fb5fde26306a76b81a7bcf04f090
Ranjeesh Kaippada <125818267+FiendKnight@users.noreply.github.com>
2023-09-02 09:04:16 +0530

    Update printname.cpp

--- Recursion/basic/printname.cpp
+++ Recursion/basic/printname.cpp
@@ -1,1 +1,20 @@
 
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void printname(int n,string name){
+    if(n==0)
+        return;
+    cout<<n<<". "<<name<<endl;
+    printname(n-1,name);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    string name;
+    cin>>name;
+    printname(n,name);
+    return 0;
+}
